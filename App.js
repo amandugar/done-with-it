98b09ed015c9645e385f5daad0bef6f5842770f1
amandugar <amanamandugar@gmail.com>
2021-10-26 17:15:11 +0530

    first-commit

--- App.js
+++ App.js
@@ -1,21 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react"
+import AppPicker from "./app/components/AppPicker"
+import AppText from "./app/components/AppText"
+import AppTextInput from "./app/components/AppTextInput"
+import Screen from "./app/components/Screen"
+import ListingsScreen from "./app/screens/ListingsScreen"
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <Screen>
+      <AppTextInput icon={"email"} placeholder="username" />
+      <AppPicker icon="apps" placeholder={"Category"} />
+    </Screen>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
